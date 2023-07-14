f03ad4a17482ea7991657c62a6713cd7f3c6578a
gitignore <91794352+octocatcode@users.noreply.github.com>
2023-07-14 12:18:50 +0300

    new-functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,80 @@
 window.addEventListener('DOMContentLoaded', function () {
     let welcomeOverlay = document.getElementById('welcome-overlay');
     let closeButton = document.getElementById('close-button');
-
-
     closeButton.addEventListener('click', function () {
+        document.body.style.overflow = "auto";
         welcomeOverlay.style.display = 'none';
     });
 });
+
+document.getElementById("main-action-button").onclick = function () {
+    document.getElementById("products").scrollIntoView({behavior: "smooth"});
+}
+
+let links = document.querySelectorAll(".menu-item > a");
+for (let i = 0; i < links.length; i++) {
+    links[i].onclick = function () {
+        document.getElementById(links[i].getAttribute('data-link')).scrollIntoView({behavior: "smooth"});
+    }
+}
+
+let buttons = document.getElementsByClassName("product-button");
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function () {
+        document.getElementById("order").scrollIntoView({behavior: "smooth"});
+    }
+}
+
+let burger = document.getElementById("burger");
+let name = document.getElementById("name");
+let phone = document.getElementById("phone");
+let orderOverlay = document.getElementById("order-overlay");
+let orderOverlayClose = document.getElementById("order-close")
+document.getElementById("order-action").onclick = function () {
+    let hasError = false;
+    [burger, name, phone].forEach(item => {
+        if (!item.value) {
+            item.parentElement.style.background = "red";
+            hasError = true;
+        } else {
+            item.parentElement.style.background = "";
+        }
+    });
+
+    if (!hasError) {
+        [burger, name, phone].forEach(item => {
+            item.value = "";
+        });
+        orderOverlay.style.display = "flex";
+        document.body.style.overflow = "hidden";
+        orderOverlayClose.onclick = function () {
+            document.body.style.overflow = "auto";
+            orderOverlay.style.display = "none";
+        }
+
+    }
+}
+let prices = document.getElementsByClassName("products-item-price");
+document.getElementById("change-currency").onclick = function (e) {
+    let currentCurrency = e.target.innerText;
+    let newCurrency = "$";
+    let coefficient = 1;
+
+    if (currentCurrency === "$") {
+        newCurrency = "₽";
+        coefficient = 90;
+    } else if (currentCurrency === "₽") {
+        newCurrency = "BYN";
+        coefficient = 3;
+    } else if (currentCurrency === 'BYN') {
+        newCurrency = '€';
+        coefficient = 0.9;
+    } else if (currentCurrency === '€') {
+        newCurrency = '¥';
+        coefficient = 6.9;
+    }
+    e.target.innerText = newCurrency;
+    for (let i = 0; i < prices.length; i++) {
+        prices[i].innerText = +(prices[i].getAttribute("data-base-price") * coefficient).toFixed(1) + " " + newCurrency;
+    }
+}
